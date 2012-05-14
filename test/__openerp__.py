4715440261a9113c1ed8b04ba6c373d7f1b06713
Kolja <mephju@gmail.com>
2012-05-14 18:02:07 +0200

    created a basic module which is not tested yet

---
+++ test/__openerp__.py
@@ -0,0 +1,13 @@
+{
+        "name" : "test",
+        "version" : "0.1",
+        "author" : "Kolja Kirchner",
+        "website" : "http://androidshopify.wordpress.com",
+        "category" : "Products",
+        "description": """  """,
+        "depends" : ['base','product'],
+        "init_xml" : [ ],
+        "demo_xml" : [ ],
+        "update_xml" : ['view.xml'],
+        "installable": True
+}
