b6715dea39dec5690444e3a175dd673c1b7ead96
Kolja <mephju@gmail.com>
2012-06-17 18:42:09 +0200

    replaced LWC-EPI menu item with configuration wizard which is started directly after installation.

--- amee.py
+++ amee.py
@@ -3,19 +3,29 @@ from osv import osv, fields
 
 
 
-class amee(osv.osv):
-    _name = 'amee.config'
-    _columns = { 
-            
+class amee_config(osv.osv_memory):
+    _name       = 'amee.config'
+    _inherit    = 'res.config'
+    _columns    = { 
             'api_key': fields.char('AMEE API Key', size=50, required=True),
             "password": fields.char("AMEE API Password", size=50, required=True),
             "url": fields.char('AMEE Webservice URL', size=100, required=True)
                    }
     
+    def execute(self, cr, uid, ids, context=None):
+        print 'config'
+        print context
+        
+        conf = self.browse(cr, uid, ids[0], context=context)
+        
+        print conf.url
+        
+        data = self.read(cr, uid, ids, context=context)
+        wizard = self.browse(cr, uid, ids, context=context)[0]
+        proxy = self.pool.get('ir.values')
 
-
-
-amee()
+        return {'type' : 'ir.actions.act_window_close'}
+amee_config()
 
 
 
--- company.py
+++ company.py
@@ -6,13 +6,7 @@ class ResCompany(osv.osv):
 
     _inherit = 'res.company'
     _columns = {
-        'ups_license_key':fields.char('UPS License Key', size=100),
-        'ups_user_id':fields.char('UPS User Name', size=100),
-        'ups_password':fields.char('UPS User Password', size=100),
-        'ups_shipper_no':fields.char('UPS Shipper Number', size=100),
-        'ups_test': fields.boolean('Is Test ?'),
-        'ups_weight_uom': fields.many2one('product.uom', 'UPS UOM for Weights'),
-        'ups_length_uom': fields.many2one('product.uom', 'UPS UOM for Lengths'),
+        'pass': fields.char('AMEE API password', size=50),
         'api_key': fields.char('AMEE API Key', size=50),
         "url": fields.char('AMEE Webservice URL', size=100)
     }
